694ac9b1f7adee9f83c0f00a15ba8a1cb1303722
Konstantin <krdavleev@sbercloud.ru>
2020-09-22 13:12:51 +0300

    ver

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='wasd',
-    version='1.0.87',
+    version='1.0.88',
     description='The Kostyan Selenium Wrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
