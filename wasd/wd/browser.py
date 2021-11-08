ccd24e716412d652ca7a6f335b9986858ed81edb
Artyom LV <artlv@Artyoms-MacBook-Pro.local>
2021-11-08 20:15:21 +0300

    added double_click browser method

--- wasd/wd/browser.py
+++ wasd/wd/browser.py
@@ -404,11 +404,12 @@ class Browser:
             element (Element): Элемент
 
         Examples:
-            >>> source = browser.find(Element("#loggout"))
-            >>> browser.double_click(source)
+            >>> browser.double_click(on_element)
         """
         log_step(f"Double click {element}")
-        ActionChains(self._driver_instance).double_click(element).perform()
+        ActionChains(self._driver_instance).double_click(
+                    self._match_first_or_fail(element)
+                    ).perform()
 
     def js_click(self, element):
         """
